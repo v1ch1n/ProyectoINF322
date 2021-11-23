119795240d719ba86c80e0ff9052f07ddd24ac34
Yi-Zhou-Zhou <65474467+Yi-Zhou-Zhou@users.noreply.github.com>
2021-11-22 23:05:36 -0300

    Add files via upload

---
+++ src/components/DataInfo/DataInfo.jsx
@@ -0,0 +1,12 @@
+import "./DataInfo.css"
+
+export default function DataInfo() {
+    return (
+        <div className="elementWrapper">Hola
+            <div>Hola</div>
+            <div>Hola</div>
+            <div>Hola</div>
+            <div>Hola</div>
+        </div>
+    )
+}
---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Topbar.css";
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Logo</span>
+                </div>
+                <div className="topRight">
+                    <img src="https://www.occ.com.mx/blog/wp-content/uploads/2021/01/Que-hace-un-project-manager-OCCMundial.jpg" alt="profile pic" className="topAvatar" />
+                
+                </div>
+                
+        
+            </div>
+        </div>
+    )
+}
